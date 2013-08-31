2d4f2682da44a289adb32e634fc01edebe2e8f4c
Kevin Colten <kevincolten@gmail.com>
2013-08-31 14:16:49 -0700

    Initial Commit/Auth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AccAdultEd::Application.config.secret_token = '9ed58277b82efa225e4bdf4f6e53496a020700d7861a639dba5d66dc6cdda56b2e34c4222305c0ca4b50c2b14ee085c5a01ebb9c63cbd3b62b64f5fc0b093e17'
